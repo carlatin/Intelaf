986c76d07e83d6a98308f4a94c778ef133908a45
carlatin <robinsonemanuelopez@gmail.com>
2019-05-10 06:45:27 -0600

    pagina de datos

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,18 +4,37 @@ angular.module('starter.controllers', [])
   //categories
   $scope.categoria=[{
     categoria:'Telefonia',
-    icono:'fas fa-mobile-alt'
-  
-    /*"Impresoras",
-    "Gaming",
-    "Computadoras",
-    "Accesorios",
-    "Componentes PC",
-    "Muebles y Oficinas",
-    "Camaras"*/
+    icono:'fas fa-mobile-alt',
+  },{
+    categoria:'Impresoras',
+    icono:'fas fa-print',
+  },{
+    categoria:'Gaming',
+    icono:'fas fa-gamepad',
+  },{
+    categoria:'Computadoras',
+    icono:'fas fa-laptop',
+  },{
+    categoria:'Accesorios',
+    icono:'fas fa-sitemap',
+  },{
+    categoria:'Componentes PC',
+    icono:'fas fa-puzzle-piece',
+  },{
+    categoria:'Muebles y Oficinas',
+    icono:'fas fa-couch',
+  },{
+    categoria:'Camaras',
+    icono:'fas fa-camera',
   }];
 })
-
+/*"Impresoras",
+"Gaming",
+"Computadoras",
+"Accesorios",
+"Componentes PC",
+"Muebles y Oficinas",
+"Camaras"*/
 .controller('ChatsCtrl', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
