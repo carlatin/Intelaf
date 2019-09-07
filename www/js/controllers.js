808f3ec105b0fc66ecd533cb8456167b7ac7a12c
carlatin <robinsonemanuelopez@gmail.com>
2019-09-07 10:58:15 -0600

    base ful

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,44 +1,54 @@
  var firebaseConfig = {
-    apiKey: "AIzaSyBcUlqowYiD3hhe0UXN-i_qj_PAMMl4X9g",
-    authDomain: "intelaf-6f196.firebaseapp.com",
-    databaseURL: "https://intelaf-6f196.firebaseio.com",
-    projectId: "intelaf-6f196",
-    storageBucket: "intelaf-6f196.appspot.com",
-    messagingSenderId: "601010648955",
-    appId: "1:601010648955:web:62da6f52e091fb5a"
+	apiKey: "AIzaSyBcUlqowYiD3hhe0UXN-i_qj_PAMMl4X9g",
+	authDomain: "intelaf-6f196.firebaseapp.com",
+	databaseURL: "https://intelaf-6f196.firebaseio.com",
+	projectId: "intelaf-6f196",
+	storageBucket: "intelaf-6f196.appspot.com",
+	messagingSenderId: "601010648955",
+	appId: "1:601010648955:web:62da6f52e091fb5a"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
 
 angular.module('starter.controllers', [])
 
-.controller('categoriesCtrl', function($scope) {
+.controller('categoriesCtrl', function($scope,$state,$rootScope) {
   //categories
   $scope.categoria=[{
-    categoria:'Telefonia',
-    icono:'fas fa-mobile-alt',
+	categoria:'Telefonia',
+	icono:'fas fa-mobile-alt',
   },{
-    categoria:'Impresoras',
-    icono:'fas fa-print',
+	categoria:'Impresoras',
+	icono:'fas fa-print',
   },{
-    categoria:'Gaming',
-    icono:'fas fa-gamepad',
+	categoria:'Gaming',
+	icono:'fas fa-gamepad',
   },{
-    categoria:'Computadoras',
-    icono:'fas fa-laptop',
+	categoria:'Computadoras',
+	icono:'fas fa-laptop',
   },{
-    categoria:'Accesorios',
-    icono:'fas fa-sitemap',
+	categoria:'Accesorios',
+	icono:'fas fa-sitemap',
   },{
-    categoria:'Componentes PC',
-    icono:'fas fa-puzzle-piece',
+	categoria:'Componentes PC',
+	icono:'fas fa-puzzle-piece',
   },{
-    categoria:'Muebles y Oficinas',
-    icono:'fas fa-couch',
+	categoria:'Muebles y Oficinas',
+	icono:'fas fa-couch',
   },{
-    categoria:'Camaras',
-    icono:'fas fa-camera',
+	categoria:'Camaras',
+	icono:'fas fa-camera',
   }];
+  $scope.funcion=function(i){
+   /* $scope.a=false;
+	if (i==true) {
+	  $scope.a=true;
+	 console.log($scope.a);
+	}*/
+	$rootScope.categorySelected = i;
+	$state.go("tab.productos");
+
+  }
 })
 /*"Impresoras",
 "Gaming",
@@ -47,11 +57,18 @@ angular.module('starter.controllers', [])
 "Componentes PC",
 "Muebles y Oficinas",
 "Camaras"*/
-.controller('productocontrol',function($scope){
-   var starCountRef = firebase.database().ref('/Mercaderia');
-      starCountRef.on('value', function(snapshot) {
-    console.log(snapshot.val());
-})
+.controller('productocontrol',function($scope,$rootScope){
+	console.log($rootScope.categorySelected)
+	$scope.mercaderiaObj = [];
+	var starCountRef = firebase.database().ref('/Mercaderia');
+		starCountRef.on('value', function(snapshot) {
+			snapshot.forEach(function(y) {
+				$scope.mercaderiaObj.push(y.val());
+				console.log(y.val())
+			})
+	})
+
+
 
 })
 .controller('ChatsCtrl', function($scope, Chats) {
@@ -65,7 +82,7 @@ angular.module('starter.controllers', [])
 
   $scope.chats = Chats.all();
   $scope.remove = function(chat) {
-    Chats.remove(chat);
+	Chats.remove(chat);
   };
 })
 
@@ -75,6 +92,6 @@ angular.module('starter.controllers', [])
 
 .controller('AccountCtrl', function($scope) {
   $scope.settings = {
-    enableFriends: true
+	enableFriends: true
   };
 });
